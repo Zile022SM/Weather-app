333784c96888705b813a64e1f1ffe8804eb2cd5a
Zile022SM <zile.kuzminac85@gmail.com>
2024-05-20 22:08:55 +0200

    Weather model

--- app/Models/CityTemperature.php
+++ app/Models/CityTemperature.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CityTemperature extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'city',
+        'temperature',
+        'created_at',
+    ];
+
+    protected $table = "weather";
 }
